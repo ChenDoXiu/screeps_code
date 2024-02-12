c6839348f1533ec2f16a3389ccb12f77f37de9fa
ChenDoXiu <2839673621@qq.com>
2024-02-12 22:30:56 +0800

    11

--- code/main.js
+++ code/main.js
@@ -1,7 +1,3 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-
 module.exports.loop = function () {
     console.log("test")
 }
